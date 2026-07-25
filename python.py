1277a47b3bd43f9da2e2b7c5c40e37405849ef7d
Ameya <ameyaraut2708@gmail.com>
2026-07-26 03:32:02 +0530

    Refactor variable names and formatting in calculator
    Updated variable names for consistency and improved formatting.

--- python.py
+++ python.py
@@ -1,13 +1,13 @@
 # CALCULATOR
 A=input("enter an operator (+, -, *, /): ")
-B=float(input("choose your first number "))
-C=float(input("choose your second number "))
+B=float(input("choose your First number "))
+C=float(input("choose your Second number "))
 
 if A== "+":
     RESULT=(B+C)
     print (f" here is your result:{RESULT}")
 elif A== "-":
-    RESULT=B-C
+    RESULT=(B-C)
     print (f" here is your result:{RESULT}")
 elif A== "*":
     RESULT=(B*C)
